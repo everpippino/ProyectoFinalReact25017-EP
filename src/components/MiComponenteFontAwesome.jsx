14a61065a89de246a3707733e93a066da984cb21
Ever Pippino <everpippino@gmail.com>
2025-05-18 12:26:00 -0300

    Componentes agregados de after clase 07

---
+++ src/components/MiComponenteFontAwesome.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+// ejemplo de como usar un componente con font awesome
+function MiComponenteFontAwesome() {
+  return (
+    <FontAwesomeIcon icon={faCoffee} />
+  );
+}
+
+export default MiComponenteFontAwesome
